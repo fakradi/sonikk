e6c7941db6bd17479ddd7ff4119e261f61f5046b
fakradi <56883399+fakradi@users.noreply.github.com>
2019-12-23 20:34:51 +0700

    Add files via upload

---
+++ sonikk.py
@@ -0,0 +1,28 @@
+echo "mail gans tq"
+
+echo "ini bkn tools penghapus data"
+
+
+echo "tapi gw mau kasih undangan perdamian"
+
+
+echo "kalo mau chat wa gw aja"
+
+
+
+echo "08994268126
+echo "tuh kontak gw"
+
+
+echo "Saya Tn.kabur (mail icm)ingin meminta maaf kepada seluruh cyber indonesia baik masalah kecil atau besar 
+Saya melakukan ini dikarena nanti akan ada perang cyber besar2an 
+Dan saya ikut untuk melakukan perang itu
+
+"Di mohon untuk kerja sama nya kita buktikan kita bisa menang melawan cyber2 lain "
+"Dan kita buktikan cyber Indonesia  bisa menjadi lebih baik"
+
+TTD:Mail
+#share"
+echo "share ke kawan kalian"
+echo "kalian mau wa gw gk"
+echo "08994268126 tuh"
